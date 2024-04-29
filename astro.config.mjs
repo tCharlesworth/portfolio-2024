a2fb61121c388bc0a3bc7246122ff1cf3b693270
Tim Charlesworth <tim_charlesworth@outlook.com>
2024-04-29 10:15:14 -0600

    panel image row

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,11 +17,14 @@ export default defineConfig({
         page: "components/storyblok/pageTypes/page",
         experience: "components/storyblok/pageTypes/experience",
         project: "components/storyblok/pageTypes/project",
+
         panel_columns: "components/storyblok/panels/panel_columns",
         panel_hero: "components/storyblok/panels/panel_hero",
         panel_project_detail: "components/storyblok/panels/panel_project_detail",
         panel_section: "components/storyblok/panels/panel_section",
         panel_text: "components/storyblok/panels/panel_text",
+        panel_image_row: "components/storyblok/panels/panel_image_row",
+
         widget_button: "components/storyblok/widgets/widget_button",
         widget_list: "components/storyblok/widgets/widget_list",
       },
