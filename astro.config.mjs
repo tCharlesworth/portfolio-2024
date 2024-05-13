6e22e498b075ee52dbac3625cf8bca51fa2bb020
Tim Charlesworth <tim_charlesworth@outlook.com>
2024-05-13 10:15:19 -0600

    panel experience listing

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,10 @@ export default defineConfig({
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       apiOptions: {
-        region: "us"
+        region: "us",
+      },
+      bridge: {
+        preventClicks: true,
       },
       components: {
         page: "components/storyblok/pageTypes/page",
@@ -24,6 +27,7 @@ export default defineConfig({
         panel_section: "components/storyblok/panels/panel_section",
         panel_text: "components/storyblok/panels/panel_text",
         panel_image_row: "components/storyblok/panels/panel_image_row",
+        panel_experience_listing: "components/storyblok/panels/panel_experience_listing",
 
         widget_button: "components/storyblok/widgets/widget_button",
         widget_list: "components/storyblok/widgets/widget_list",
--- src/utils/storyblok.js
+++ src/utils/storyblok.js
@@ -6,3 +6,9 @@ export function isAssetDefined(asset) {
   }
   return false
 }
+export function ParseStoryblokDate(sbDate) {
+  // From Storyblok: "2015-06-01 12:00"
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+  // To parse function: YYYY-MM-DDTHH:mm:ss.sssZ
+  return new Date(Date.parse(sbDate.replace(' ', 'T')+'Z'))
+}
